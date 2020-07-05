df3ea053b875d245d1ae29e64b336f8c96926cf7
Michelle Abbas <michellehyabbas@gmail.com>
2020-07-06 09:07:26 +1200

    Bug

--- js/header.js
+++ js/header.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
 	// Background animation
 
-	var wave = document.querySelector(".wave1 path")
-	var wave2 = document.querySelector(".wave2 path")
-	var wave3 = document.querySelector(".wave3 path")
+	// var wave = document.querySelector(".wave1 path")
+	// var wave2 = document.querySelector(".wave2 path")
+	// var wave3 = document.querySelector(".wave3 path")
 
-	var tl = gsap.timeline({repeat: -1, yoyo: true})
+	// var tl = gsap.timeline({repeat: -1, yoyo: true})
 
-	tl
-	.to(wave, {duration: 5, morphSVG: ".wave-morph1 path", ease: "power1.inOut"}, 1)
-	.to(wave2, {duration: 7, morphSVG: ".wave-morph2 path", ease: "power1.inOut"}, 1)
-	.to(wave3, {duration: 9, morphSVG: ".wave-morph3 path", ease: "power1.inOut"}, 1);
+	// tl
+	// .to(wave, {duration: 5, morphSVG: ".wave-morph1 path", ease: "power1.inOut"}, 1)
+	// .to(wave2, {duration: 7, morphSVG: ".wave-morph2 path", ease: "power1.inOut"}, 1)
+	// .to(wave3, {duration: 9, morphSVG: ".wave-morph3 path", ease: "power1.inOut"}, 1);
 
 	// Slide and Fade animation
 
