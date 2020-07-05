93cda15999760c0f30dde2b84c520e86f2470f68
Michelle Abbas <michellehyabbas@gmail.com>
2020-07-05 22:47:52 +1200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -11,53 +11,51 @@ $(document).ready(function() {
 	tl
 	.to(wave, {duration: 5, morphSVG: ".wave-morph1 path", ease: "power1.inOut"}, 1)
 	.to(wave2, {duration: 7, morphSVG: ".wave-morph2 path", ease: "power1.inOut"}, 1)
-	.to(wave3, {duration: 9, morphSVG: ".wave-morph3 path", ease: "power1.inOut"}, 1)
-	.from(".slide-in", {duration: 0.7, autoAlpha:0, x: -100, ease: "power1.inOut"}, 0)
-	.from(".fade-in", {duration: 0.3, autoAlpha:0, delay:0.7, ease: "power1.inOut"}, 0)
+	.to(wave3, {duration: 9, morphSVG: ".wave-morph3 path", ease: "power1.inOut"}, 1);
 
 	// Slide and Fade animation
 
-	// var slideincontroller = new ScrollMagic.Controller();
-	// var fadeincontroller = new ScrollMagic.Controller();
-	// var slidein = Array.prototype.slice.call(document.querySelectorAll(".slide-in"));
-	// var fadein = Array.prototype.slice.call(document.querySelectorAll(".fade-in"));
-	// var self = this;
+	var slideincontroller = new ScrollMagic.Controller();
+	var fadeincontroller = new ScrollMagic.Controller();
+	var slidein = Array.prototype.slice.call(document.querySelectorAll(".slide-in"));
+	var fadein = Array.prototype.slice.call(document.querySelectorAll(".fade-in"));
+	var self = this;
 
-	// slidein.forEach(function(self) {
+	slidein.forEach(function(self) {
 
-	// 	var slideintween = gsap.from(self, {duration: 0.7, autoAlpha:0, x: -100, ease: "power1.inOut"});
+		var slideintween = gsap.from(self, {duration: 0.7, autoAlpha:0, x: -100, ease: "power1.inOut"});
 
-	// 	var slideinscene = new ScrollMagic.Scene({triggerElement: self, triggerHook: 'onEnter', reverse: false})
-	// 	.setTween(slideintween)
-	// 	.setClassToggle(self, "active")
-	// 	.addTo(slideincontroller)
+		var slideinscene = new ScrollMagic.Scene({triggerElement: self, triggerHook: 'onEnter', reverse: false})
+		.setTween(slideintween)
+		.setClassToggle(self, "active")
+		.addTo(slideincontroller)
 
-	// });
+	});
 
-	// fadein.forEach(function(self) {
+	fadein.forEach(function(self) {
 
-	// 	var fadeintween = gsap.from(self, {duration: 0.3, autoAlpha:0, delay:0.7, ease: "power1.inOut"});
+		var fadeintween = gsap.from(self, {duration: 0.3, autoAlpha:0, delay:0.7, ease: "power1.inOut"});
 
-	// 	var fadeinscene = new ScrollMagic.Scene({triggerElement: self, triggerHook: 'onEnter', reverse: false})
-	// 	.setTween(fadeintween)
-	// 	.setClassToggle(self, "active")
-	// 	.addTo(fadeincontroller)
+		var fadeinscene = new ScrollMagic.Scene({triggerElement: self, triggerHook: 'onEnter', reverse: false})
+		.setTween(fadeintween)
+		.setClassToggle(self, "active")
+		.addTo(fadeincontroller)
 
-	// });
+	});
 
 	// SVG icon animation
 
-	// var hand = document.querySelector("#hand")
-	// var star1 = document.querySelector("#star1")
-	// var star2 = document.querySelector("#star2")
-	// var star3 = document.querySelector("#star3")
+	var hand = document.querySelector("#hand")
+	var star1 = document.querySelector("#star1")
+	var star2 = document.querySelector("#star2")
+	var star3 = document.querySelector("#star3")
 
-	// var icontl = gsap.timeline({repeat: -1, repeatDelay: 1})
+	var icontl = gsap.timeline({repeat: -1, repeatDelay: 1})
 
-	// icontl
-	// .from(hand, {duration: 1.2, x: -60, ease: "power1.inOut"}, 0)
-	// .from(star1, {duration: 0.3, autoAlpha:0, ease: "power1.inOut"}, 0.3)
-	// .from(star2, {duration: 0.3, autoAlpha:0, ease: "power1.inOut"}, 0.6)
-	// .from(star3, {duration: 0.3, autoAlpha:0, ease: "power1.inOut"}, 0.9);
+	icontl
+	.from(hand, {duration: 1.2, x: -60, ease: "power1.inOut"}, 0)
+	.from(star1, {duration: 0.3, autoAlpha:0, ease: "power1.inOut"}, 0.3)
+	.from(star2, {duration: 0.3, autoAlpha:0, ease: "power1.inOut"}, 0.6)
+	.from(star3, {duration: 0.3, autoAlpha:0, ease: "power1.inOut"}, 0.9);
 
 });
