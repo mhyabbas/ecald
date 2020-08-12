04b0b1f4cee745b224023aeff75a6043b03c33bc
Michelle Abbas <michellehyabbas@gmail.com>
2020-08-12 19:50:47 +1200

    search

--- js/header.js
+++ js/header.js
@@ -70,24 +70,24 @@ $(document).ready(function() {
 
 	// Search filter
 
-	if($('.search select').length) {
-		$(function () {
-			$(".search select").multipleSelect({
-				selectAll: false,
-				ellipsis: true,
-				onClick: function () {
-					var count = $("#search-comp + .ms-parent li.selected").length;
-					$("#search-comp + .ms-parent li:first-of-type label").attr('data-before', count + ' selected');
-				}
-			});
+	// if($('.search select').length) {
+	// 	$(function () {
+	// 		$(".search select").multipleSelect({
+	// 			selectAll: false,
+	// 			ellipsis: true,
+	// 			onClick: function () {
+	// 				var count = $("#search-comp + .ms-parent li.selected").length;
+	// 				$("#search-comp + .ms-parent li:first-of-type label").attr('data-before', count + ' selected');
+	// 			}
+	// 		});
 
-			$("#search-comp + .ms-parent li:first-of-type label").attr('data-before', '0 selected'); 
+	// 		$("#search-comp + .ms-parent li:first-of-type label").attr('data-before', '0 selected'); 
 
-			$("input[value='Clear']").click(function(){
-				$("#search-comp").multipleSelect('uncheckAll');
-				$("#search-comp + .ms-parent li:first-of-type label").attr('data-before', '0 selected');  
-			});
-		})
-	}
+	// 		$("input[value='Clear']").click(function(){
+	// 			$("#search-comp").multipleSelect('uncheckAll');
+	// 			$("#search-comp + .ms-parent li:first-of-type label").attr('data-before', '0 selected');  
+	// 		});
+	// 	})
+	// }
 
 });
