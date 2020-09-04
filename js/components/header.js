2844efc0dc0ccf38243a640d4c29ad3befc38ff5
Michelle Abbas <michellehyabbas@gmail.com>
2020-09-04 19:55:40 +1200

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -54,19 +54,19 @@ $(document).ready(function() {
 		var c3 = $('#timeline circle.tl0');
 		var c4 = $('#timeline path.tl1');
 
-		gsap.set(c1, {autoAlpha: 0, transformOrigin: 'center center'});
-		gsap.set(c2, {autoAlpha: 0, transformOrigin: 'center center'});
-		gsap.set(c3, {autoAlpha: 0, transformOrigin: 'center center'});
-		gsap.set(c4, {autoAlpha: 0, transformOrigin: 'center center'});
+		gsap.set(c1, {transformOrigin: 'center center'});
+		gsap.set(c2, {transformOrigin: 'center center'});
+		gsap.set(c3, {transformOrigin: 'center center'});
+		gsap.set(c4, {transformOrigin: 'center center'});
 
 		var timelinetl = gsap.timeline({repeat: -1, yoyo: true});
 
 		timelinetl
 		.fromTo(line, {drawSVG: "0% 0%"}, {duration: 1, drawSVG: "0% 100%"}, 0)
-		.fromTo(c1, {scale: 0 }, {duration: 1, autoAlpha: 1, scale: 1, stagger: 0.2}, 1)
-		.fromTo(c2, {scale: 0.4 }, {duration: .5, autoAlpha: 1, scale: 1, stagger: 0.2}, 1.5)
-		.fromTo(c3, {scale: 0.4 }, {duration: .5, autoAlpha: 1, scale: 1, stagger: 0.2}, 2)
-		.fromTo(c4, {scale: 0.4 }, {duration: .5, autoAlpha: 1, scale: 1, stagger: 0.2}, 2.5)
+		.fromTo(c1, {autoAlpha: 0, scale: 0 }, {duration: 1, autoAlpha: 1, scale: 1}, 1)
+		.fromTo(c2, {autoAlpha: 0, scale: 0.4 }, {duration: .5, autoAlpha: 1, scale: 1}, 1.5)
+		.fromTo(c3, {autoAlpha: 0, scale: 0.4 }, {duration: .5, autoAlpha: 1, scale: 1}, 2)
+		.fromTo(c4, {autoAlpha: 0, scale: 0.4 }, {duration: .5, autoAlpha: 1, scale: 1}, 2.5)
 	}
 
 	// Header slide and fade animation
