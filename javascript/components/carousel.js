9fdff7ea125abb94d749ec2d63aa5fa8465a68ff
Michelle Abbas <michellehyabbas@gmail.com>
2020-10-11 17:36:02 +1300

    Curve

--- javascript/components/carousel.js
+++ javascript/components/carousel.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	// Carousel
+
 	var options = {
 		arrows: true,
 		speed: 500,
@@ -12,4 +14,42 @@ $(document).ready(function(){
 	}
 
 	$('.carousel').slick(options);
+
+	// Carousel - Full width with bleeding
+
+	var fulloptions = {
+		autoplay: true,
+		centerMode: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		speed: 500,
+		prevArrow: '<a class="btn arrow prev" href=""><i class="material-icons">&#xe5c4;</i></a>', 
+		nextArrow: '<a class="btn arrow next" href=""><i class="material-icons">&#xe5c8;</i></a>',
+		responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 700,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				slidesToShow: 1
+			}
+		}
+		]
+	};
+
+	if($('.carousel-full .card').length > 1) {
+		fulloptions.dots = true;
+	}
+
+	$('.carousel-full').slick(fulloptions);
+
 });
